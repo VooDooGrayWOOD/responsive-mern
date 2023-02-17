6c802dcc7d17c059989a47bc3a572c761ae3c273
VooDooGrayWOOD <voodoograywood@gmail.com>
2023-02-17 09:31:00 +0700

    editing the backend

--- server/index.js
+++ server/index.js
@@ -8,7 +8,15 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import {register} from "./controllers/auth.js"
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import { verifyToken } from "./middleware/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { register } from "./controllers/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
 /* Configurations */
 
@@ -37,9 +45,16 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
-/* Routes With Files */ 
+/* Routes With Files */
 
-app.post("/auth/register", upload.single("picture"), register)
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
+
+/* Routes */
+
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 /* Mongoose Setup */
 
@@ -52,5 +67,9 @@ mongoose
     })
     .then(() => {
         app.listen(PORT, () => console.log(`Server port: ${PORT}...`));
+
+        /* Add Data One Time */
+        // User.insertMany(users);
+        // Post.insertMany(posts);
     })
     .catch((error) => console.log(`${error} did not connect`));
