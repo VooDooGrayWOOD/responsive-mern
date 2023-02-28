aecc7a26e95a11eed859e81399a3c89e8a607915
VooDooGrayWOOD <voodoograywood@gmail.com>
2023-02-28 12:03:50 +0700

    add navbar

---
+++ client/src/components/FlexBetween.jsx
@@ -0,0 +1,10 @@
+import { Box } from '@mui/material'
+import { styled } from '@mui/system'
+
+const FlexBetween = styled(Box)({
+    dispaly: 'flex',
+    justifyContent: 'space-between',
+    ailgnItems: 'center'
+})
+
+export default FlexBetween
--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,5 +1,36 @@
+import { Box, Typography, useTheme, useMediaQuery } from '@mui/material'
+import Form from './Form'
+
 const LoginPage = () => {
-    return <div>loginpage</div>
+    const theme = useTheme()
+    const isNonMobileScreens = useMediaQuery('(min-width:1000px')
+
+    return (
+        <Box>
+            <Box
+                width="100%"
+                backgroundColor={theme.palette.background.alt}
+                p="1re, 6%"
+                textAlign="center"
+            >
+                <Typography fontWeight="bold" fontSize="32px" color="primary">
+                    SocioMedia
+                </Typography>
+            </Box>
+            <Box
+                width={isNonMobileScreens ? '50%' : '93%'}
+                p="2rem"
+                m="2rem auto"
+                borderRadius="1.5rem"
+                backgoundColor={theme.palette.background.alt}
+            >
+                <Typography fontWeight="500" variant="h5" sx={{ mb: '1.5rem' }}>
+                    Welcome to SocioMedia, the Social Media for Sociopath
+                </Typography>
+                <Form></Form>
+            </Box>
+        </Box>
+    )
 }
 
 export default LoginPage
