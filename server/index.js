03393b47b183b96875d54e6d3d02a8420bac41be
VooDooGrayWOOD <voodoograywood@gmail.com>
2023-03-08 10:34:36 +0700

    edit script on package.json

--- server/index.js
+++ server/index.js
@@ -60,6 +60,16 @@ app.use('/posts', postRoutes)
 
 mongoose.set('strictQuery', false)
 const PORT = process.env.PORT || 6001
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve(__dirname, '..', 'client', 'build')))
+    app.get('*', (req, res) => {
+        res.sendFile(
+            path.resolve(__dirname, '..', 'client', 'build', 'index.html')
+        )
+    })
+}
+
 mongoose
     .connect(process.env.MONGO_URL, {
         useNewUrlParser: true,
