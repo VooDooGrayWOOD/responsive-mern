8b0acf2aecc0c1fa48aaf4feae7bc359a907b6f5
Sergey <89291563+VooDooGrayWOOD@users.noreply.github.com>
2024-10-23 22:14:46 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,6 +24,12 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 dotenv.config()
 const app = express()
+
+app.use((req, res, next) => {
+    res.setHeader("Content-Security-Policy", "default-src 'self'; connect-src 'self' http://localhost:3001");
+    next();
+});
+
 app.use(express.json())
 app.use(helmet())
 app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }))
